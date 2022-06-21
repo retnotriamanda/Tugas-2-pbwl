c6a9350c7dd68afd08371b11ff3924b4be88eab2
retnotriamanda <106569691+retnotriamanda@users.noreply.github.com>
2022-06-22 00:01:30 +0700

    Create dashboard.php

---
+++ tugas-2-retno/pages/dashboard.php
@@ -0,0 +1,25 @@
+<div class="container" style="padding-top: 40px; min-height: 530px">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Tugas 2</h5>
+            <p class="card-text">
+                <div class="row">
+                    <div class="col-md-1">
+                        Nama 
+                    </div>
+                    <div class="col-md-3">
+                        : Retno Tri Amanda
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-1">
+                        NIM
+                    </div>
+                    <div class="col-md-3">
+                        : 0702193166
+                    </div>
+                </div>
+            </p>
+        </div>
+    </div>
+</div>
